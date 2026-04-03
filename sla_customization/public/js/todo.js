46f34ce36bf354f503bcb780326b6aa12d56f4e4
Sneha Singh <sneha.singh@cloverinfotech.com>
2026-04-03 12:20:35 +0530

    Updated incorrect path from overide to sla_customization

--- sla_customization/public/js/todo.js
+++ sla_customization/public/js/todo.js
@@ -2,7 +2,7 @@ frappe.ui.form.on('HD Ticket', {
     onload: function(frm) {
         frm.set_query("custom_assigned_to", function() {
             return {
-                query: "override.api.user_query.active_user_query"
+                query: "sla_customization.api.user_query.active_user_query"
             };
         });
     }
